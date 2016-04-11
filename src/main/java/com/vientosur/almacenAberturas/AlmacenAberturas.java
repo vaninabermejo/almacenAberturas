29ba5be26c90422e5a0ad36f3986f5e6ab80ffce
vaninabermejo <vanibermejo@gmail.com>
2016-04-11 16:11:42 -0300

    first commit

---
+++ src/main/java/com/vientosur/almacenAberturas/AlmacenAberturas.java
@@ -0,0 +1,54 @@
+package com.vientosur.almacenAberturas;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.annotations.Viewport;
+import com.vaadin.annotations.Widgetset;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+/**
+ * This UI is the application entry point. A UI may either represent a browser window 
+ * (or tab) or some part of a html page where a Vaadin application is embedded.
+ * <p>
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
+ * overridden to add component to the user interface and initialize non-component functionality.
+ */
+@Viewport("user-scalable=no,initial-scale=1.0")
+@Theme("almacenAberturastheme")
+@Widgetset("com.example.almacenAberturas.AlmacenAberturasWidgetset")
+public class AlmacenAberturas extends UI {
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        final VerticalLayout layout = new VerticalLayout();
+        
+        final TextField name = new TextField();
+        name.setCaption("Type your name here:");
+
+        Button button = new Button("Click Me");
+        button.addClickListener( e -> {
+            layout.addComponent(new Label("Thanks " + name.getValue() 
+                    + ", it works!"));
+        });
+        
+        layout.addComponents(name, button);
+        layout.setMargin(true);
+        layout.setSpacing(true);
+        
+        setContent(layout);
+    }
+
+    @WebServlet(urlPatterns = "/*", name = "AlmacenAberturasServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = AlmacenAberturas.class, productionMode = false)
+    public static class AlmacenAberturasServlet extends VaadinServlet {
+    }
+}
---
+++ target/.generated/com/google/gwt/lang/com_00046example_00046almacenAberturas_00046AlmacenAberturasWidgetset__EntryMethodHolder.java
@@ -0,0 +1,6 @@
+package com.google.gwt.lang;
+public class com_00046example_00046almacenAberturas_00046AlmacenAberturasWidgetset__EntryMethodHolder {
+  public static final void init() {
+    // to be synthesized later
+  }
+}
